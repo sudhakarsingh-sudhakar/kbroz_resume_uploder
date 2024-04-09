82c93742a7a4ecc88a4d13aef9321b9c694672ff
NIRAJ.KUMAR <NIRAJ.KUMAR@ORAHI.COM>
2024-04-09 18:51:49 +0530

    new changes

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 from app import app, db
-from flask import request, jsonify, make_response, render_template , send_file
+from flask import request, jsonify, make_response, render_template , send_file, redirect, url_for, flash
 from util.utility import *
 from app.models import User, UploadedFile
 from werkzeug.security import check_password_hash
 from io import BytesIO
 
+
+
+SEARCH =  None
 def login():
     data = request.json
     response_data = {}
@@ -70,8 +73,12 @@ def login_route():
         # Generate JWT token and set cookie
         token = generate_token(email)
         response = make_response(jsonify({'status': True, 'message': 'Login successful', 'token': token}), 200)
+
         response.set_cookie('jwt_token', token, httponly=True, secure=True)
-        return render_template('homepage.html')
+        print(response)
+        # return redirect(url_for('index'))
+        return render_template('homepage.html', token = token)
+        #return response
     else:
         # User with provided email and password does not exist
         return render_template('index.html', message='Invalid email or password'), 401
@@ -103,12 +110,12 @@ def upload_file():
 # download file on basis of keyword
 @app.route('/login/download', methods=['GET'])
 def download_file():
-    keyword = request.args.get('keyword')
+    id = request.args.get('id')
 
-    if not keyword:
-        return jsonify({'error': 'Keyword is required.'}), 400
+    if not id:
+        return jsonify({'error': 'id is required.'}), 400
 
-    uploaded_file = UploadedFile.query.filter_by(keyword=keyword).first()
+    uploaded_file = UploadedFile.query.filter_by(id=id).first()
     if not uploaded_file:
         return jsonify({'error': 'File not found for the given keyword.'}), 404
 
@@ -119,7 +126,7 @@ def download_file():
     return send_file(
         file_stream,
         as_attachment=True,
-        download_name=f'{keyword}.pdf',
+        download_name=f'{id}.pdf',
         mimetype='application/pdf'
     )
     
@@ -127,12 +134,102 @@ def download_file():
 
 @app.route('/login/all_files', methods=['GET'])
 def list_files():
-    partial_keyword = request.args.get('keyword')
-
+    partial_keyword = request.args.get('search')
+    global SEARCH 
+    
     if not partial_keyword:
-        return jsonify({'error': 'Keyword is required.'}), 400
+        flash("please provide a vaild keyword")
+        #return jsonify({'error': 'Keyword is required.'}), 400
+        return render_template("homepage.html")
+    
+    SEARCH = partial_keyword
 
     files = UploadedFile.query.filter(UploadedFile.keyword.like(f'{partial_keyword}%')).all()
-    files_data = [{'id': file.id, 'keyword': file.keyword,'file_name': file.file_name} for file in files]
-    return jsonify(files_data)
+    files_data = [{'id': file.id, 'keyword': file.keyword,'file_name': file.file_name,'user':file.user} for file in files]
+    #return files_data
+    return render_template("search_result.html" ,search_results =files_data)
 
+
+
+blacklisted_tokens = set()
+@app.route('/logout')
+def logout():
+    # Get the JWT token from the request cookies
+    print(request)
+    token = request.cookies.get('jwt_token')
+    
+
+    if not token:
+        return render_template("index.html")
+
+
+    # Check if the token is blacklisted (optional, depending on your implementation)
+    if token in blacklisted_tokens:
+        # return jsonify({'status': False, 'message': 'Token already revoked'}), 401
+        return render_template("index.html")
+
+    # Add the token to the blacklist (optional)
+    blacklisted_tokens.add(token)
+
+    # Remove the JWT token cookie from the response
+    response = make_response(jsonify({'status': True, 'message': 'Logout successful'}), 200)
+    response.set_cookie('jwt_token', '', expires=0)
+
+    return render_template("index.html")
+
+
+
+@app.route('/login/upload_update/<int:id>', methods=['POST'])
+def update_file(id):
+    print(request.form)
+    keyword = request.form.get('keyword')
+    user = request.form.get("user")
+    file = request.files.get('file')  # Use get() to safely get the file, which can be None
+    file_name = request.form.get('file_name')
+    print(f"keyword {keyword}")
+
+    # if not (keyword and file):
+    #     return jsonify({'error': 'Keyword and file are required.'}), 400
+
+    # Query the database to find the uploaded file record based on the provided id
+    uploaded_file = UploadedFile.query.get(id)
+
+    if not uploaded_file:
+        flash("uploaded file not find")
+        return jsonify({'error': 'Uploaded file not found.'}), 404
+
+    # Update the keyword and name fields if provided
+    uploaded_file.keyword = keyword
+    uploaded_file.user = user
+    uploaded_file.file_name = file_name
+
+    # Update the file data if a new file is provided
+    if file:
+        uploaded_file.file_name = file.filename
+        uploaded_file.file_data = file.read()
+    files = UploadedFile.query.filter(UploadedFile.keyword.like(f'{SEARCH}%')).all()
+    files_data = [{'id': file.id, 'keyword': file.keyword,'file_name': file.file_name,'user':file.user} for file in files]
+
+    db.session.commit()
+
+    return render_template("search_result.html",  search_results =files_data)
+
+
+@app.route('/login/delete/<int:id>')
+def delete(id):
+    uploaded_file = UploadedFile.query.get(id)
+
+    if not uploaded_file:
+        flash("uploaded file not find")
+        return jsonify({'error': 'Uploaded file not found.'}), 404
+    
+    # Delete the uploaded file from the database
+    db.session.delete(uploaded_file)
+    files = UploadedFile.query.filter(UploadedFile.keyword.like(f'{SEARCH}%')).all()
+    files_data = [{'id': file.id, 'keyword': file.keyword,'file_name': file.file_name,'user':file.user} for file in files]
+
+    db.session.commit()
+
+    return jsonify(message= 'Uploaded file deleted successfully.', serach_result= files_data), 200
+    
+    
